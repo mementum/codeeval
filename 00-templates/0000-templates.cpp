8ba2b7574faa68b3c7ff2ae452c53e4189e089a5
mementum <mementum@users.noreply.github.com>
2015-12-02 14:33:34 +0100

    Add Stream Imbuer for easy parsin to C++ template

--- 00-templates/0000-templates.cpp
+++ 00-templates/0000-templates.cpp
@@ -59,6 +59,23 @@ void debugout(T t, Args... args) // recursive variadic function
 
 
 ///////////////////////////////////////////////////////////////////////////////
+// Stream Imbuer for Parsing
+// http://stackoverflow.com/questions/1894886/parsing-a-comma-delimited-stdstring
+///////////////////////////////////////////////////////////////////////////////
+struct csv_reader: std::ctype<char> {
+    csv_reader(): std::ctype<char>(get_table()) {}
+    static std::ctype_base::mask const* get_table() {
+        static std::vector<std::ctype_base::mask>
+            rc(table_size, std::ctype_base::mask());
+
+        rc[','] = std::ctype_base::space;
+        rc['\n'] = std::ctype_base::space;
+        rc[' '] = std::ctype_base::space;
+        return &rc[0];
+    }
+};
+
+///////////////////////////////////////////////////////////////////////////////
 // String utilities
 ///////////////////////////////////////////////////////////////////////////////
 template<typename T, typename C>
