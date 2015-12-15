e98998fb1271e89373e7638c55b41bd055722747
mementum <mementum@users.noreply.github.com>
2015-12-15 11:24:08 +0100

    0000-template.cpp: pre C++11 custom STL allocator with policies

--- 00-templates/0000-template.cpp
+++ 00-templates/0000-template.cpp
@@ -40,7 +40,6 @@
 #include <cmath>
 #include <cstdint>
 
-// STL Allocator
 ///////////////////////////////////////////////////////////////////////////////
 // Typename Printer (GCC and CLang)
 #include <string>
@@ -62,6 +61,7 @@ std::string type_name()
 
 
 ///////////////////////////////////////////////////////////////////////////////
+// STL C++11 Allocator
 template <class T>
 struct Mallocator
 {
@@ -83,6 +83,147 @@ struct Mallocator
     }
 };
 
+
+/*
+  Keeps track of the allocated chunks in terms of size and delivers from the
+  cache accordingly
+*/
+struct MemCacheDynamic {
+    size_t total++;
+    std::map<size_t, std::vector<void *>> memcache;
+
+    ~MemCacheDynamic() {
+        for(auto &&nlist: memcache)
+            for(auto &&p: nlist.second)
+                ::free(p);
+    }
+
+    void *alloc(size_t n) {
+        auto &&nlist = memcache[n];
+        if(not nlist.empty()) {
+            auto p = nlist.back();
+            nlist.pop_back();
+            return p;
+        }
+        total++;
+        return ::malloc(n);
+    }
+
+    void dealloc(void *p, size_t n) { memcache[n].push_back(p);}
+};
+
+
+/*
+  Pops / Pushes to the cache (a vector) allocated/deallocated memory. Meant for
+  maps which always request the same size (GCC 4.8.x, GCC 4.9.x)
+*/
+struct MemCacheOne {
+    size_t total;
+    std::vector<void *> memcache;
+
+    ~MemCacheOne() {
+        for(auto &&p: memcache)
+            ::free(p);
+    }
+
+    void *alloc(size_t n) {
+        if(not memcache.empty()) {
+            auto p = memcache.back();
+            memcache.pop_back();
+            return p;
+        }
+        total++;
+        return ::malloc(n);
+    }
+
+    void dealloc(void *p, size_t n) { memcache.push_back(p);}
+};
+
+/*
+  Direct allocation/deallocation - meant for comparison with other custom
+  allocators
+*/
+
+struct MemCacheDirect {
+    size_t total;
+    void *alloc(size_t n) { total++; return ::malloc(n); }
+    void dealloc(void *p, size_t n) { ::free(p); }
+};
+
+
+/*
+  Supporting pre C++11 allocators
+
+  Specific STL custom allocator mostly for Maps with the MemCacheOne
+  suballocating struct
+
+  This relies on the fact that maps do a 1 by 1 allocation (at least in the GCC
+  implementation 4.8.x and 4.9.x) which allows a very quick push/pop scheme of
+  allocated and to be reused, pointers with no size check.
+
+  Applying it to the maps in "glue" there have been memory and speed gains of
+  about 10%.
+
+  If the suballocator is MemCacheOne, the size is taken into account and the
+  allocator can be applied to anything. It has been tested with the vectors
+  defined in "glue", but unfortunately no memory gains have been seen in this
+  case.
+
+  Note: Using forward_list increases memory. Using stack increases memory by
+  orders of magnitude (GCC 4.8.1)
+*/
+template<typename T, typename M>
+struct Mallocator
+{
+    using MemCache = M;
+
+    // typedefs
+    typedef T value_type;
+    // Pre C++11
+    typedef value_type *pointer;
+    typedef const value_type *const_pointer;
+    typedef value_type &reference;
+    typedef const value_type &const_reference;
+    typedef std::size_t size_type;
+    typedef std::ptrdiff_t difference_type;
+
+    static MemCache &getcache() {
+        static MemCache memcache;
+        return memcache;
+    };
+
+    MemCache &mcache;
+
+    // Constructor
+    Mallocator() noexcept : mcache(getcache()) {}
+
+    // A converting copy constructor:
+    T *allocate(const size_t n) {
+        return static_cast<T *>(mcache.alloc(n * sizeof(T)));
+    }
+
+    // Deallocate
+    void deallocate(T *p, size_t n) noexcept { mcache.dealloc(p, n); }
+
+    template<class U> bool operator ==(const Mallocator<U, M> &) const noexcept { return true; }
+    template<class U> bool operator !=(const Mallocator<U, M> &) const noexcept { return false; }
+
+    // Boilerplat pre C++11
+    template<class U> struct rebind { typedef Mallocator<U, M> other; };
+
+    size_type max_size() const { return std::numeric_limits<size_type>::max() / sizeof(T); }
+
+    pointer address(reference r) { return &r; }
+    const_pointer address(const_reference r) { return &r; }
+
+    void construct(pointer p, const T &t) { new(p) T(t); }
+    void destroy(pointer p) { p->~T(); }
+};
+
+
+template<typename T> using MAllocDyn = Mallocator<T, MemCacheDynamic>;
+template<typename T> using MAllocOne = Mallocator<T, MemCacheOne>;
+template<typename T> using MAllocDirect = Mallocator<T, MemCacheDirect>;
 
 ///////////////////////////////////////////////////////////////////////////////
 // Output debugging
