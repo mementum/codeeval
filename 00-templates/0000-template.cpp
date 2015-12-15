8349ddee8ca6df494f0685adb3b6b2f88c63838b
mementum <mementum@users.noreply.github.com>
2015-12-15 11:23:35 +0100

    0000-template.cpp: typename printer for gcc

--- 00-templates/0000-template.cpp
+++ 00-templates/0000-template.cpp
@@ -41,6 +41,27 @@
 #include <cstdint>
 
 // STL Allocator
+///////////////////////////////////////////////////////////////////////////////
+// Typename Printer (GCC and CLang)
+#include <string>
+#include <cstdlib>
+#include <cxxabi.h>
+
+template<typename T>
+std::string type_name()
+{
+    int status;
+    std::string tname = typeid(T).name();
+    char *demangled_name = abi::__cxa_demangle(tname.c_str(), NULL, NULL, &status);
+    if(status == 0) {
+        tname = demangled_name;
+        std::free(demangled_name);
+    }
+    return tname;
+}
+
+
+///////////////////////////////////////////////////////////////////////////////
 template <class T>
 struct Mallocator
 {
