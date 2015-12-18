d1a6fcf4d18a93af059f33fef6cd386de9fcf4a3
mementum <mementum@users.noreply.github.com>
2015-12-18 10:28:48 +0100

    Apply separator reader to beautify

--- 02-medium/0223-alternative-reality/0223-alternative-reality.cpp
+++ 02-medium/0223-alternative-reality/0223-alternative-reality.cpp
@@ -18,53 +18,72 @@
 // Headers for test case
 #include <fstream>
 #include <iostream>
-#include <string>
 
 
 // Headers for the implementation
 #include <algorithm>
-#include <vector>
-#include <utility>
+#include <iterator>
 
-const std::vector<size_t> COINS = {50, 25, 10, 5};
+
+const size_t COINS[] = {50, 25, 10, 5};
+const auto CEND = std::end(COINS);
 
 
 template<typename ntype, typename iter>
 auto
-alternative_reality(ntype cents, iter itcoin)
+calc_changes(ntype cents, iter itcoin)
 {
-    if (not cents or itcoin == COINS.end())
+    if (not cents or itcoin == CEND)
         return 1;
 
     auto coin = *itcoin;
     auto coincount = cents / coin;
     auto changes = 0;
-    for (auto i = 0; i <= coincount; i++)
-        changes += alternative_reality(cents - (coin * i), std::next(itcoin));
+    for (auto i=0; i <= coincount; i++)
+        changes += calc_changes(cents - coin * i, std::next(itcoin));
 
     return changes;
 }
 
 
-template<typename ntype>
+template<typename iter, typename iterout>
 auto
-alternative_reality(ntype cents)
+alternative_reality(iter in, iterout out)
 {
-    auto itcoin = std::find_if(COINS.begin(),
-                               COINS.end(),
-                               [&cents] (size_t coin) { return coin <= cents;});
+    auto itcoin = std::find_if(
+        std::begin(COINS), CEND,
+        [&in] (size_t coin) { return coin <= *in;});
 
-    return alternative_reality(cents, itcoin);
+    *out++ = calc_changes(*in, itcoin);
 }
+
+
+struct SeparatorReader: std::ctype<char>
+{
+    template<typename T>
+    SeparatorReader(const T &seps): std::ctype<char>(get_table(seps), true) {}
+
+    template<typename T>
+    std::ctype_base::mask const *get_table(const T &seps) {
+        auto &&rc = new std::ctype_base::mask[std::ctype<char>::table_size]();
+        for(auto &&sep: seps)
+            rc[static_cast<unsigned char>(sep)] = std::ctype_base::space;
+        return &rc[0];
+    }
+};
 
 
 int
 main(int argc, char *argv[]) {
     std::ifstream stream(argv[1]);
-    std::string line;
+    stream.imbue(std::locale(stream.getloc(), new SeparatorReader("\n")));
 
-    while (std::getline(stream, line)) {
-        std::cout << alternative_reality(std::stoi(line), COINS.begin()) << std::endl;
-    }
+    auto out = std::ostream_iterator<size_t>(std::cout, "\n");
+    auto in1 = std::istream_iterator<size_t>(stream);
+    auto in2 = std::istream_iterator<size_t>();
+
+    for(auto &&in=in1; in != in2; in++)
+        alternative_reality(in, out);
+
     return 0;
 }
