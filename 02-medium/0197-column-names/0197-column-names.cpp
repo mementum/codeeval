0713a170ea4c577eb1e65ed64596ed2abd3b955b
mementum <mementum@users.noreply.github.com>
2015-12-31 00:00:43 +0100

    0197-column-names c++

---
+++ 02-medium/0197-column-names/0197-column-names.cpp
@@ -0,0 +1,56 @@
+/*
+  Copyright (C) 2015 Daniel Rodriguez
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+// Headers for test case
+#include <fstream>
+#include <iostream>
+
+// Headers for the implementation
+#include <algorithm>
+#include <iterator>
+#include <vector>
+
+///////////////////////////////////////////////////////////////////////////////
+// Main
+///////////////////////////////////////////////////////////////////////////////
+int
+main(int argc, char *argv[]) {
+    std::ifstream stream(argv[1]);
+
+    const int ALPHALEN = 26;
+    const char OFFSET = 'A';
+
+    auto in1 = std::istream_iterator<int>(stream);
+    auto in2 = std::istream_iterator<int>();
+    auto out = std::ostream_iterator<char>(std::cout);
+
+    for(auto in=in1; in != in2; in++) {
+        auto colnames = std::vector<char>();
+        auto colnum = *in - 1;
+
+        while(colnum >= 0) {
+            auto rem = colnum % ALPHALEN;
+            colnames.push_back(OFFSET + rem);
+            colnum = colnum / ALPHALEN - 1;
+        }
+
+        std::copy(colnames.rbegin(), colnames.rend(), out);
+        std::cout << std::endl;
+    }
+
+    return 0;
+}
