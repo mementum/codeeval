1a644c068118ff0e327bd31d16ef9f2867a3e058
mementum <mementum@users.noreply.github.com>
2016-01-03 15:17:09 +0100

    0086-poker-hands py and readme updates

--- 03-hard/0086-poker-hands/0086-poker-hands.py
+++ 03-hard/0086-poker-hands/0086-poker-hands.py
@@ -21,103 +21,75 @@
 from __future__ import (absolute_import, division, print_function,)
 #                        unicode_literals)
 
-import itertools
 import sys
 
 # Small Py2/3 compatibility layer
 if sys.version_info.major == 2:
-    filter = itertools.ifilter
-    map = itertools.imap
-    range = xrange
-    zip = itertools.izip
-
+    pass
 else:  # >= 3
     def cmp(a, b): return (a > b) - (a < b)
 
 
-class PokerHand(object):
-    CARDS = '23456789TJQKA'
-    HIGHVAL = len(CARDS)
+SCORE_STRAIGHT_FLUSH = 8000000
+SCORE_FOUR_OF_A_KIND = 7000000
+SCORE_FULL_HOUSE = 6000000
+SCORE_FLUSH = 5000000
+SCORE_STRAIGHT = 4000000
+SCORE_THREE_OF_A_KIND = 3000000
+SCORE_TWO_PAIR = 2000000
+SCORE_PAIR = 1000000
+SCORE_HIGH_CARD = 0
 
-    SCORE_STRAIGHT_FLUSH = 8000000
-    SCORE_FOUR_OF_A_KIND = 7000000
-    SCORE_FULL_HOUSE = 6000000
-    SCORE_FLUSH = 5000000
-    SCORE_STRAIGHT = 4000000
-    SCORE_THREE_OF_A_KIND = 3000000
-    SCORE_TWO_PAIRS = 2000000
-    SCORE_PAIR = 1000000
-    SCORE_HIGH_CARD = 0
+STDECK = '2345A23456789TJQKA'
+DECK = '23456789TJQKA'
 
-    def __init__(self, cards):
-        kinds, suits = zip(*cards)  # separate face values from suits
 
-        # flush check
-        flush = suits.count(suits[0]) == len(suits)
+def hand_eval(hand):
+    vals, suits = ''.join(sorted(hand[0::3], key=DECK.index)), hand[1::3]
 
-        # count number of same face values
-        kcounts = [kinds.count(x) for x in self.CARDS]
+    flush = suits.count(suits[0]) == len(suits)
+    straight = STDECK.find(vals)
 
-        # group the kinds of counts (zeros, ones, twos, threes, fours)
-        kgroups = [kcounts.count(x) for x in range(len(cards))]
+    if straight >= 0:
+        score = SCORE_STRAIGHT_FLUSH * flush or SCORE_STRAIGHT
+        score += straight
 
-        # To value high cards within individual hands - if needed be
-        ones = [i for i, x in enumerate(kcounts) if x == 1]
+    else:
+        rank = sorted(map(lambda v: (vals.count(v), DECK.index(v)), set(vals)))
+        lowrank, lidx = rank[-2]
+        highrank, hidx = rank[-1]
 
-        # Assign scoring
-        if kgroups[1] == 5:  # straight, flush, high card
-            # check straights and a2345 straight
-            if ones[4] - ones[0] == 4:
-                score = (self.SCORE_STRAIGHT_FLUSH * flush or
-                         self.SCORE_STRAIGHT)
+        if highrank == 4:
+            score = SCORE_FOUR_OF_A_KIND
+            score += hidx
 
-                score += ones[4]
+        elif highrank == 3:
+            score = SCORE_FULL_HOUSE * (lowrank == 2) or SCORE_THREE_OF_A_KIND
+            score += hidx
 
-            elif ones[3] == 3 and kcounts[-1]:  # a2345
-                score = (self.SCORE_STRAIGHT_FLUSH * flush or
-                         self.SCORE_STRAIGHT)
+        elif highrank == 2:
+            score = SCORE_TWO_PAIR * (lowrank == 2) or SCORE_PAIR
+            score += sum([pow(13, i) * kc[1] for i, kc in enumerate(rank)])
 
-                score += ones[3]
+        else:
+            score = SCORE_FLUSH * flush or SCORE_HIGH_CARD
+            score += sum([pow(13, i) * kc[1] for i, kc in enumerate(rank)])
 
-            else:
-                score = self.SCORE_FLUSH * flush or self.SCORE_HIGH_CARD
-                score += self.value_cards(ones)
-
-        elif kgroups[4]:  # four of kind
-            score = self.SCORE_FOUR_OF_A_KIND
-            score += kcounts.index(4)
-
-        elif kgroups[3]:  # full house, three of kind
-            score = (self.SCORE_FULL_HOUSE * kgroups[2] or
-                     self.SCORE_THREE_OF_A_KIND)
-            score += kcounts.index(3)
-
-        else:  # one or two pairs
-            score = (self.SCORE_TWO_PAIRS * (kgroups[2] == 2) or
-                     self.SCORE_PAIR)
-            twos = [i for i, x in enumerate(kcounts) if x == 2]
-            for t, two in enumerate(twos):
-                score += pow(self.HIGHVAL, t + 3) * two
-            score += self.value_cards(ones)
-
-        self.score = score
-
-    def value_cards(self, cards):
-        # Complete valuation with position of ones
-        return sum([pow(self.HIGHVAL, i) * card
-                    for i, card in enumerate(cards)])
+    return score
 
 
 if __name__ == '__main__':
     test_cases = open(sys.argv[1], 'r')
+
+    results = {1: 'left', -1: 'right', 0: 'none'}
+
     for line in test_cases:
-        hands = line.rstrip('\n').split()
-        retcmp = cmp(PokerHand(hands[:5]).score, PokerHand(hands[5:]).score)
-        if retcmp < 0:
-            print('right')
-        elif retcmp > 0:
-            print('left')
-        else:
-            print('none')
+        hands = line.rstrip('\n')
+        nhands = ((len(hands) + 1) // 3) // 2
+        lh = hands[0:nhands * 2 + nhands - 1]
+        rh = hands[nhands * 2 + nhands:]
+
+        retcmp = cmp(hand_eval(lh), hand_eval(rh))
+        print(results[retcmp])
 
     test_cases.close()
