6f7ae4467488f8cc7a5e34a93cd28e0008cf3f1e
mementum <mementum@users.noreply.github.com>
2016-02-12 16:50:45 +0100

    0019-bit-positions c++/readme

---
+++ 01-easy/0019-bit-positions/0019-bit-positions.cpp
@@ -0,0 +1,59 @@
+/*
+  Copyright (C) 2016 Daniel Rodriguez
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+// Headers for test case input/output
+#include <fstream>
+#include <iostream>
+
+
+///////////////////////////////////////////////////////////////////////////////
+// Stream Imbuer for Parsing
+// http://stackoverflow.com/questions/1894886/parsing-a-comma-delimited-stdstring
+//    ss.imbue(std::locale(ss.getloc(), new SeparatorReader(" ,|")));
+///////////////////////////////////////////////////////////////////////////////
+
+struct SeparatorReader: std::ctype<char>
+{
+    template<typename T>
+    SeparatorReader(const T &seps): std::ctype<char>(get_table(seps), true) {}
+
+    template<typename T>
+    std::ctype_base::mask const *get_table(const T &seps) {
+        auto &&rc = new std::ctype_base::mask[std::ctype<char>::table_size]();
+        for(auto &&sep: seps)
+            rc[static_cast<unsigned char>(sep)] = std::ctype_base::space;
+        return &rc[0];
+    }
+};
+
+///////////////////////////////////////////////////////////////////////////////
+// Main
+///////////////////////////////////////////////////////////////////////////////
+int
+main(int argc, char *argv[]) {
+    std::ifstream stream(argv[1]);
+    // Only ',' is separator. '\n'/eof break int conversion
+    stream.imbue(std::locale(stream.getloc(), new SeparatorReader(",\n")));
+
+    int n, p1, p2;
+    while(stream >> n >> p1 >> p2) {
+        auto b1 = (n >> (p1 - 1) & 1);
+        auto b2 = (n >> (p2 - 1) & 1);
+        std::cout << (b1 ^ b2 ? "false" : "true") << std::endl;
+    }
+    return 0;
+}
